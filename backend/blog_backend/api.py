04bfaf1109de1929d27c4bdf6b9c138c6cc9aba4
LinkunGao <gaolinkun123@gmail.com>
2021-11-22 02:02:28 +1300

    install beautiful soup4, git the edit box content,and get the img src attribute content

--- backend/blog_backend/api.py
+++ backend/blog_backend/api.py
@@ -1,6 +1,25 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+# 导入爬虫包
+from bs4 import BeautifulSoup
+import requests
 
 @api_view(['POST'])
 def add_article(request):
+    title = request.POST['title']
+    describe = request.POST['describe']
+    cover = request.POST['cover']
+    content = request.POST['content']
+    
+    # 利用爬虫工具解析富文本文档
+    soup = BeautifulSoup(content, 'html.parser')
+    # 获取所有img标签 图片
+    imgList = soup.find_all('img')
+    
+    for index in range(0,len(imgList)):
+        # print(type(imgList[index]))
+        # 获取每个img标签下的src属性， 以便之后下载
+        print(imgList[index]['src'])
+
+
     return Response('ok')
